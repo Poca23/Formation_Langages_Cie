0df11c90a02e7543886d01e30a1b4c88045fd80f
Naudin Claire <cndweb37@gmail.com>
2024-10-28 15:41:24 +0100

    mvFilesFormationOpenclassroomsJavascript

--- OpenClassrooms/apprendreAProgrammerEnJavascript/Partie3FaitesInteragirJavaScriptAvecUnePageWeb/modifierUnElementDUnePageWeb/modifierUnElementDUnePageWeb.js
+++ OpenClassrooms/apprendreAProgrammerEnJavascript/Partie3FaitesInteragirJavaScriptAvecUnePageWeb/modifierUnElementDUnePageWeb/modifierUnElementDUnePageWeb.js
@@ -4,3 +4,13 @@
 
 let baliseImage = document.getElementById("premiereImage");
 baliseImage.setAttribute("alt", "Ceci est la nouvelle valeur de alt");
+
+// baliseImage.alt = "Ceci est une image de test modifiée";
+// baliseImage.classList.add("nouvelleClasse");
+// baliseImage.classList.remove("photo");
+
+baliseImage = document.getElementById("premiereImage");
+baliseImage.setAttribute("alt", "Ceci est une image de test modifiee ouf..");
+baliseImage.src = "baccBlackSmoke.jpg";
+baliseImage.classList.add("nouvelleClasse");
+baliseImage.classList.remove("photoFlexCenter");
