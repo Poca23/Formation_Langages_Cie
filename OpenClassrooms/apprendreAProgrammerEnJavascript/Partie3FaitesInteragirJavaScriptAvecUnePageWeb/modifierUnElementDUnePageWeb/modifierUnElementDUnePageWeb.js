f12cee5638df2a74a4bd6eed96186a13acdabe08
Naudin Claire <cndweb37@gmail.com>
2024-10-28 12:11:22 +0100

    mvFilesProjectsJavascript

---
+++ OpenClassrooms/apprendreAProgrammerEnJavascript/Partie3FaitesInteragirJavaScriptAvecUnePageWeb/modifierUnElementDUnePageWeb/modifierUnElementDUnePageWeb.js
@@ -0,0 +1,6 @@
+// ////////////////////////////////
+// Modifiez un élément d’une page web
+// ////////////////////////////////
+
+let baliseImage = document.getElementById("premiereImage");
+baliseImage.setAttribute("alt", "Ceci est la nouvelle valeur de alt");
