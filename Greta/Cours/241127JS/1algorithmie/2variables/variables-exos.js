7175b11798a336e3b161b33984c1b4eb59eb832f
Claire Naudin <cndweb37@gmail.com>
2024-11-27 12:23:50 +0100

    mvFolderFormationGretaCoursJs

---
+++ Greta/Cours/241127JS/1algorithmie/2variables/variables-exos.js
@@ -0,0 +1,20 @@
+ /**
+   * -------------------------------------------------------
+   * 1 - Renommer les variables correctement
+   * -------------------------------------------------------
+*/
+const country = "France";
+console.log(country); // m'affiche "France"
+
+
+let z = "Salut je m'appelle Bruno et voici ma description";
+
+const métier = "Analyste financier au Yémen";
+
+const age = 23;
+
+l = "Bordeaux";
+
+arr = "Une liste de nombres";
+
+?? = "le prénom du plus grand agent secret de tous les temps";
