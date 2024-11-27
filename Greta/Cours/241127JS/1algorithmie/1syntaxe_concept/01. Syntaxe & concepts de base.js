f4c2235e40fcd8e56bafd084f2d4766626342ba8
Claire Naudin <cndweb37@gmail.com>
2024-11-27 11:52:33 +0100

    addFilesFormationGretaCoursJs

---
+++ Greta/Cours/241127JS/1algorithmie/1syntaxe_concept/01. Syntaxe & concepts de base.js
@@ -0,0 +1,44 @@
+// console.log() est un fonction JavaScript
+console.log("Hello, World!");
+console.log("coucou hibou coucou hibou");
+
+// afficher des erreurs
+console.error("non non, c'est pas par là");
+
+// afficher des avertissements
+console.warn("Encore moins par là");
+
+// erreur dans la synthaxe
+console.loge("bla bla");
+
+// opérations arithmétiques
+let age = 23;
+
+// Incrémenter
+age = age + 1;
+age += 1;
+age++;
+
+// Décrémenter
+age = age - 1;
+age -= 1;
+age--;
+
+// Comparaison : true or false
+
+1 == 1; // true, valeur égale
+"Bob" === "Bob"; // true valeur et type égales
+"Bob" === "bob"; // false valeur et type différents
+1 === "1"; // false valeur et type différents
+
+1 != 2; // true, valeur différentes
+1 != 1; //false, valeur égales
+
+1 !== 2; // true, valeur et types différentes
+1 !== 1; //false, valeur et types égales
+
+2 > 1; //true, supérieur à ..
+2 >= 2; //true, supérieur ou égal à ..
+
+2 < 1; //true, inférieur à ..
+2 <= 2; //true, inférieur ou égal à ..
