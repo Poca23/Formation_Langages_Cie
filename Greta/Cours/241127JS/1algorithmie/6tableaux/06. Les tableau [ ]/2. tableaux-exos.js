6400bc43fcb86c361dee6a23db3f25be37c8297c
Claire Naudin <cndweb37@gmail.com>
2024-11-27 21:03:46 +0100

    addFileFormationGretaCoursJs

---
+++ Greta/Cours/241127JS/1algorithmie/6tableaux/06. Les tableau [ ]/2. tableaux-exos.js
@@ -0,0 +1,78 @@
+// Voici un jeu de données : interdiction d'y toucher :) 
+const characterNames = [
+  "63AIS",
+  "A'misandra",
+  "Amimari",
+  "Alika",
+  "54B2",
+  "A'merpact",
+  "Amazora",
+];
+
+/**
+ * -------------------------------------------------------
+ * 1 - Afficher le nombre de personnages dans le tableau 
+ * -------------------------------------------------------
+ */
+
+/**
+ * -------------------------------------------------------
+ * 2 - Afficher le nom du premier personnage du tableau
+ * -------------------------------------------------------
+ */
+
+/**
+ * -------------------------------------------------------
+ * 3 - Afficher le nom du dernier personnage (le 7eme)
+ * -------------------------------------------------------
+ */
+
+/**
+ * -------------------------------------------------------
+ * 4 - Afficher le nom du dernier personnage **SANS** 
+ * savoir qu'il est le 7eme
+ * -------------------------------------------------------
+ */
+
+/**
+ * -------------------------------------------------------
+ * 5 - Afficher les noms de tous les personnages
+ * -------------------------------------------------------
+ */
+
+
+/**
+ * -------------------------------------------------------
+ * 6 - Afficher le nom de chaque personnage accompagné du 
+ * nombre de caractères qu'il contient. Ex: Alika (5)
+ * -------------------------------------------------------
+ */
+
+
+/**
+ * -------------------------------------------------------
+ * 7 - Afficher le nom de chaque personnage en minuscules
+ * -------------------------------------------------------
+ */
+
+
+
+
+/**
+ * -------------------------------------------------------
+ * 8 - Afficher le nombre de personnages dont le nom 
+ * contient la lettre 'a' (minuscule ou majuscule)
+ * -------------------------------------------------------
+ */
+
+
+
+/**
+ * -------------------------------------------------------
+ * 9 - Créer une fonction `search(needle)` qui renvoie 
+ * les personnages dont le nom contient les lettres 
+ * passées en paramètre.
+ * 
+ * Ex : search('Amim') doit renvoyer 'Amimari'
+ * ------------------------------------------------------- 
+ */
