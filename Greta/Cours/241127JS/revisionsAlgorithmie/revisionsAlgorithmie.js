1358cdc491574f7ac9deacf7528611f91f6c3073
Claire Naudin <cndweb37@gmail.com>
2024-12-01 17:49:03 +0100

    solvedExosRevisionsAlgorithmieJsFormationGretaCoursJs

--- Greta/Cours/241127JS/revisionsAlgorithmie/revisionsAlgorithmie.js
+++ Greta/Cours/241127JS/revisionsAlgorithmie/revisionsAlgorithmie.js
@@ -14,6 +14,16 @@ Copier le code
 pairOuImpair(5); // Retourne : "Impair"
 pairOuImpair(8); // Retourne : "Pair"
  */
+// ÉCRIRE ICI :
+
+// function pairOuImpair(number){
+//       if (number % 2 === 0){
+//             return 'Ce nombre est pair';  // retourne le message 'Ce nombre est pair'
+//       } else {
+//             return 'Ce nombre est impair'; // retourne le message 'Ce nombre est impair'
+//       }
+// };
+// console.log(pairOuImpair(8));
 
 /**----------------------------------------------------------------------------- */
 /* 2. Somme des nombres
@@ -26,6 +36,33 @@ Copier le code
 somme([1, 2, 3, 4]); // Retourne : 10
 somme([10, -2, 3]);  // Retourne : 11 
 */
+// ÉCRIRE ICI :
+
+// function calculeSumNbArray() {
+//   const array1 = [1, 2, 3, 4];
+//   let sum = 0;
+
+//   for (let i = 0; i < array1.length; i++) {
+//     sum += array1[i];
+//   }
+//   return sum;
+// }
+// console.log(calculeSumNbArray());
+
+// Utilisation avec la boucle forEach :
+
+// const array1 = [1, 2, 3, 4];
+// function calculeSumNbArray(array1) {
+//   let somme = 0;
+
+//   array1.forEach((element) => {
+//     somme += element;
+//   });
+//   return somme;
+// }
+// console.log(calculeSumNbArray(array1));
+
+// const array2 = [10, -2, 3, 4];
 
 /**----------------------------------------------------------------------------- */
 /*
@@ -39,6 +76,63 @@ Copier le code
 maximum([1, 5, 3, 9, 2]); // Retourne : 9
 maximum([-10, -5, -3]);   // Retourne : -3
 */
+// ÉCRIRE ICI :
+
+// const array2 = [-10, -5, -3];
+
+// première méthode, FOR :
+
+// function calculHighNumber(array1) {
+//   let maxNumber = array1[0];
+
+//   for (let i = 0; i < array1.length; i++) {
+//     if (maxNumber < array1[i]) {
+//       maxNumber = array1[i];
+//     }
+//   }
+//   return maxNumber;
+// }
+// console.log(calculHighNumber(array1));
+
+// deuxième méthode, FOR :
+
+// const array1 = [1, 5, 3, 9, 2];
+
+// function calculSumNumberMax(array1) {
+//   let maxNumber = array1[0];
+
+//   array1.forEach((element) => {
+//     if (maxNumber < element) {
+//       maxNumber = element;
+//     }
+//   });
+
+//   return maxNumber;
+// }
+// console.log(calculSumNumberMax(array1));
+
+// troisième méthode, for Each ET Math.max :
+
+// const array1 = [1, 5, 3, 9, 2];
+
+// function calculMaxNumber(array1){
+//       let maxNumber = array1[0];
+
+//       array1.forEach((element) => {
+//             maxNumber = Math.max(maxNumber,element);
+//       })
+//       return maxNumber;
+// }
+// console.log(calculMaxNumber(array1));
+
+// quatrième méthode, Math.max :
+
+// const array1 = [1, 5, 3, 9, 2];
+
+// function calculMaxNumber (array1){
+//       return Math.max(...array1);
+// }
+// console.log(calculMaxNumber(array1));
 
 /**----------------------------------------------------------------------------- */
 /* 
@@ -53,6 +147,26 @@ Exemple :
 Copier le code
 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, ... 
 */
+// ÉCRIRE ICI :
+
+
+// for (let i = 1; i < 21; i++){
+
+//       if (i % 3 === 0 && i % 5 === 0){
+//             console.log("FizzBuzz");  
+
+//       }  else if (i % 3 === 0){
+//                   console.log("Fizz"); 
+//       } 
+//             else if  (i % 5 === 0){
+//                   console.log("Buzz");
+//       } 
+//             else {
+//                   console.log(i);
+//       }
+// }
+
+
 
 /**----------------------------------------------------------------------------- */
 /* 
@@ -66,7 +180,7 @@ Copier le code
 inverser("bonjour"); // Retourne : "ruojnob"
 inverser("JavaScript"); // Retourne : "tpircSavaJ"
 */
-
+// ÉCRIRE ICI :
 
 /**----------------------------------------------------------------------------- */
 /* 
@@ -80,7 +194,7 @@ Copier le code
 filtrerPairs([1, 2, 3, 4, 5, 6]); // Retourne : [2, 4, 6]
 filtrerPairs([7, 11, 14, 18]);   // Retourne : [14, 18]
 */
-
+// ÉCRIRE ICI :
 
 /**----------------------------------------------------------------------------- */
 /*
@@ -95,7 +209,7 @@ Copier le code
 factorielle(5); // Retourne : 120
 factorielle(0); // Retourne : 1
 */
-
+// ÉCRIRE ICI :
 
 /**----------------------------------------------------------------------------- */
 /**----------------------------------------------------------------------------- */
