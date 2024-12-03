b297ebb2eca9695e82f7555da725a055b357bc93
Claire Naudin <cndweb37@gmail.com>
2024-12-03 13:20:57 +0100

    solvedExoFiles3DomJsFormationGretaCoursJsDom

--- Greta/Cours/241127JS/2dom/02. Sélectionner des éléments/selectionner-element-exos.js
+++ Greta/Cours/241127JS/2dom/02. Sélectionner des éléments/selectionner-element-exos.js
@@ -1,70 +1,145 @@
 /**
-   * -------------------------------------------------------
-   * 1 - Récupérer (par les 2 méthodes différentes) la section "nos services" : 
-   * → par son id 
-   * → par son nom de balise
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 1 - Récupérer (par les 2 méthodes différentes) la section "nos services" :
+ * → par son id
+ * → par son nom de balise
+ * -------------------------------------------------------
+ */
 
-
-/**
-   * -------------------------------------------------------
-   * 2 - Récupérer les 2 inputs par leur id
-   * → Afficher leur valeur (value) dans la console
-   * -------------------------------------------------------
-*/
+// let sectionServices = document.getElementById('our-services');
+// console.log(sectionServices);
+// let baliseSection = document.getElementsByTagName('section');
+// console.log(baliseSection[1]);
 
 /**
-   * -------------------------------------------------------
-   * 3 - Récupérer le titre de niveau 3 de la seconde division de la seconde section
-   * → afficher la valeur de son texte dans la console
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 2 - Récupérer les 2 inputs par leur id
+ * → Afficher leur valeur (value) dans la console
+ * -------------------------------------------------------
+ */
 
+// let inputId1 = document.getElementById('great');
+// let inputId2 = document.getElementById('not-great');
 
-/**
-   * -------------------------------------------------------
-   * 4 - Il est possible de récupérer un élément qui possède plusieurs classes. Récupérer tous les éléments :
-   * → qui partagent la classe :   "btn"
-   * → qui partagent la classe :   "cta-btn"
-   * → qui partagent la classe :   "cta-primary"
-   * → qui partagent la classe :   "cta-secondary"
-   * → qui partagent les classes : "cta-btn cta-primary"
-   * → qui partagent les classes : "btn cta-btn cta-secondary"
-   * 
-   * → Les afficher dans la console
-   * → Via JS, compter le nombre de fois où la classe "btn" est présente
-   * -------------------------------------------------------
-*/
+// console.log(inputId1, inputId2);
 
 /**
-   * -------------------------------------------------------
-   * 5 - Récupérer tous les paragraphes. Via une boucle, les afficher dans la console
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 3 - Récupérer le titre de niveau 3 de la seconde division de la seconde section
+ * → afficher la valeur de son texte dans la console
+ * -------------------------------------------------------
+ */
+
+// let titreH3 = document.querySelector('section h3');
+// console.log(titreH3);
+
+// let texte = titreH3.textContent;
+// console.log(texte).value;
 
 /**
-   * -------------------------------------------------------
-   * 6 - Récupérer et afficher les éléments suivants dans la console : 
-   * → le parent direct de la première section
-   * → le premier enfant de la première section
-   * → le dernier enfant de la première section
-   * → le frère précédent de la section "nos services"
-   * → le frère suivant de la section "nos services"
-   * 
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 4 - Il est possible de récupérer un élément qui possède plusieurs classes. Récupérer tous les éléments :
+ * → qui partagent la classe :   "btn"
+ * → qui partagent la classe :   "cta-btn"
+ * → qui partagent la classe :   "cta-primary"
+ * → qui partagent la classe :   "cta-secondary"
+ * → qui partagent les classes : "cta-btn cta-primary"
+ * → qui partagent les classes : "btn cta-btn cta-secondary"
+ *
+ * → Les afficher dans la console
+ * → Via JS, compter le nombre de fois où la classe "btn" est présente
+ * -------------------------------------------------------
+ */
+
+// let elementClasses = document.querySelectorAll('.btn', '.cta-btn','.cta-primary', '.cta-secondary', '.cta-btn cta-primary', '.btn cta-btn cta-secondary' )
+// console.log(elementClasses);
+
+// let arrayClasses = Array.from(elementClasses);
+
+// let filterBtn = arrayClasses
+// .filter(element => element.classList.contains('btn'));
+// console.log(filterBtn);
 
 /**
-   * -------------------------------------------------------
-   * 7 - Créer une fonction qui permette de compter le nombre de fois "où la balise X apparaît".
-   * Compter ensuite le nombre de fois où : 
-   * la balise HTML h1 apparaît
-   * la balise HTML h2 apparaît
-   * la balise HTML h3 apparaît
-   * la balise HTML p apparaît
-   * la balise HTML section apparaît
-   * la balise HTML div apparaît
-   * → Calculer la somme totale de ces occurences et afficher le résultat dans la console
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 5 - Récupérer tous les paragraphes. Via une boucle, les afficher dans la console
+ * -------------------------------------------------------
+ */
+
+// let allParagraphs = document.querySelectorAll('p');
+// console.log(allParagraphs);
+
+// allParagraphs.forEach((paragraphe) => {
+//    console.log(paragraphe.innerText);
+// });
+
+/**
+ * -------------------------------------------------------
+ * 6 - Récupérer et afficher les éléments suivants dans la console :
+ * → le parent direct de la première section
+ * → le premier enfant de la première section
+ * → le dernier enfant de la première section
+ * → le frère précédent de la section "nos services"
+ * → le frère suivant de la section "nos services"
+ *
+ * -------------------------------------------------------
+ */
+
+let sectionParent = document.getElementsByTagName("section");
+console.log(sectionParent[0].parentElement);
+
+let sectionFirstEnfant = document.getElementsByTagName("section");
+console.log(sectionFirstEnfant[0].firstElementChild);
+
+let sectionLastEnfant = document.getElementsByTagName("section");
+console.log(sectionLastEnfant[0].lastElementChild);
+
+let sectionServices = document.getElementById("our-services");
+console.log(sectionServices);
+
+let sectionServicesPreviousBrother = sectionServices.previousElementSibling;
+console.log(sectionServicesPreviousBrother);
+
+let sectionServicesNextBrother = sectionServices.nextElementSibling;
+console.log(sectionServicesNextBrother);
+
+/**
+ * -------------------------------------------------------
+ * 7 - Créer une fonction qui permette de compter le nombre de fois "où la balise X apparaît".
+ * Compter ensuite le nombre de fois où :
+ * la balise HTML h1 apparaît
+ * la balise HTML h2 apparaît
+ * la balise HTML h3 apparaît
+ * la balise HTML p apparaît
+ * la balise HTML section apparaît
+ * la balise HTML div apparaît
+ * → Calculer la somme totale de ces occurences et afficher le résultat dans la console
+ * -------------------------------------------------------
+ */
+
+// créer une fonction countNbTag
+// la fonction permet de compter le nombre de balise qui apparaît
+// On utilisera la méthode document.getElementsByTagName(paramètre)
+// On crée la fonction avec comme argument une balise qui va compter le nombre de balise totale
+
+
+
+function countNbTag (tag){
+   let nbTag = document.getElementsByTagName(tag);
+ // Vérification si la collection n'est pas vide
+   if (nbTag.length > 0){
+    return nbTag.length;
+   }
+   // Si la balise n'existe pas dans le DOM, retourner 0
+   return 0;
+}
+console.log(countNbTag("p"));
+
+
+
+let counter = 0;
+
+const arrayTag = ["h1", "h2", "h3", "p", "section", "div"];
+arrayTag.forEach ((element) =>
+   counter = counter + countNbTag(element)); // Ajout du nombre de balises)
+console.log(counter);
--- Greta/Cours/241127JS/2dom/03. Modifier des éléments/modify-elements-exos.js
+++ Greta/Cours/241127JS/2dom/03. Modifier des éléments/modify-elements-exos.js
@@ -1,82 +1,177 @@
 /**
-   * -------------------------------------------------------
-   * 1 - Créer une div :
-   * → qui possède un titre de niveau 3 avec un id et du texte
-   * → qui possède un paragraphe avec du texte
-   * → qui possède un bouton avec une classe et un texte
-   * 
-   * Ajouter cette div à la section "hero-section"
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 1 - Créer une div :
+ * → qui possède un titre de niveau 3 avec un id et du texte
+ * → qui possède un paragraphe avec du texte
+ * → qui possède un bouton avec une classe et un texte
+ *
+ * Ajouter cette div à la section "hero-section"
+ * -------------------------------------------------------
+ */
+
+let div = document.createElement("div"); //on crée une div
+
+let h3 = document.createElement("h3"); // on crée un titre h3
+h3.id = "monIdh3"; // on crée un id au titre h3 précédent
+h3.innerText = "Je suis un titre h3"; // on ajoute du texte dans le titre h3 précédent
+div.appendChild(h3); // on ajoute le titre h3 à la div
+
+//Idem avec le paragraphe
+let p = document.createElement("p");
+p.innerText = "Je suis un paragraphe";
+div.appendChild(p);
+
+//Idem avec le paragraphe
+let button = document.createElement("button");
+button.classList = "btn";
+button.innerText = "Clique moi wesh :) ";
+div.appendChild(button);
+
+document.body.appendChild(div); // on ajoute la div au body du document html
+
+let sectionHero = document.querySelector("#hero-section");
+sectionHero.appendChild(div);
 
 /**
-   * -------------------------------------------------------
-   * 2 - Écrire une fonction qui créer et retourne un élément HTML "li" avec du texte.
-   * Exemple : l'execution de la fonction     createMenuItem("Item 1")       retourne :       <li>Item 1</li>
-   * → Créer ainsi 5 puces et les insérer dans la liste à puces "menu"
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 2 - Écrire une fonction qui créer et retourne un élément HTML "li" avec du texte.
+ * Exemple : l'execution de la fonction  createMenuItem("Item 1")       retourne :       <li>Item 1</li>
+ * → Créer ainsi 5 puces et les insérer dans la liste à puces "menu"
+ * -------------------------------------------------------
+ */
 
+function createMenuItem() {
+  let menu = document.querySelector("#menu");
+  for (let i = 0; i < 5; i++) {
+    let item = document.createElement("li");
+    item.innerText = "Je suis un item du menu";
 
-/**
-   * -------------------------------------------------------
-   * 3 - Créer un second menu (via JS uniquement):
-   * → La liste à puces aura pour id "menu2" et comportera 3 éléments
-   * -------------------------------------------------------
-*/
-
-/**
-   * -------------------------------------------------------
-   * 4 - Déplacer le premier et le dernier élément de la première liste (menu) dans la seconde liste (menu2)
-   * -------------------------------------------------------
-*/
+    menu.appendChild(item);
+  }
+}
+createMenuItem();
 
 /**
-   * -------------------------------------------------------
-   * 5 - Changer le texte des puces qui viennent d'être déplacées
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 3 - Créer un second menu (via JS uniquement):
+ * → La liste à puces aura pour id "menu2" et comportera 3 éléments
+ * -------------------------------------------------------
+ */
+
+let menu2 = document.createElement("menu");
+menu2.id = "menu2";
+document.body.appendChild(menu2);
+
+function createMenu2Item() {
+  let menu = document.querySelector("#menu2");
+  for (let i = 0; i < 3; i++) {
+    let item = document.createElement("li");
+    item.innerText = "Je suis un item du menu2";
+
+    menu.appendChild(item);
+  }
+}
+createMenu2Item();
 
 /**
-   * -------------------------------------------------------
-   * 6 - Supprimer le titre de niveau 1 qui n'a rien à faire dans le <head></head> et dont l'id est scandaleux 🤓
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 4 - Déplacer le premier et le dernier élément de la première liste (menu) dans la seconde liste (menu2)
+ * -------------------------------------------------------
+ */
+let menu = document.querySelector("#menu");
 
+menu2.appendChild(menu.firstElementChild);
+menu2.appendChild(menu.lastElementChild);
 
-// Voici un bout de code : ne pas toucher ! 
+/**
+ * -------------------------------------------------------
+ * 5 - Changer le texte des puces qui viennent d'être déplacées
+ * -------------------------------------------------------
+ */
+
+let items = menu2.querySelectorAll("li");
+
+if (items.length > 1) {
+  // tableau d'items doit être de minmum un élément
+  items[items.length - 1].innerText =
+    "Je suis le dernier item renommé du menu2";
+  items[items.length - 2].innerText =
+    "Je suis l'avant-dernier item renommé du menu2";
+}
+
+/**
+ * -------------------------------------------------------
+ * 6 - Supprimer le titre de niveau 1 qui n'a rien à faire dans le <head></head> et dont l'id est scandaleux 🤓
+ * -------------------------------------------------------
+ */
+
+let h1Frauduleux = document.querySelector("#test-loooool");
+
+h1Frauduleux.remove();
+
+// Voici un bout de code : ne pas toucher !
 const elementListToCreate = [
-   {
-      name: "section",
-      times: 3
-   },
-   {
-      name: "div",
-      times: 3
-   },
-   {
-      name: "p",
-      times: 1
-   },
-   {
-      name: "span",
-      times: 3
-   },
-]
+  {
+    name: "section",
+    times: 3,
+  },
+  {
+    name: "div",
+    times: 3,
+  },
+  {
+    name: "p",
+    times: 1,
+  },
+  {
+    name: "span",
+    times: 3,
+  },
+];
 /**
-   * -------------------------------------------------------
-   * 7 - Créer une boucle qui permet de créer et d'injecter dans le body les éléments du tableau un nombre X de fois. 
-   * Chaque élément est l'enfant du précédent.
-   * Exemple (basé sur le tableau ci-dessus) : il faut créer 3 sections. Chaque section possède 3 div. Chaque div possède 3 paragraphes (avec du texte). Chaque paragraphe possède 3 spans (avec du texte et une classe, la même pour chaque span)
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 7 - Créer une boucle qui permet de créer et d'injecter dans le body les éléments du tableau un nombre X de fois.
+ * Chaque élément est l'enfant du précédent.
+ * Exemple (basé sur le tableau ci-dessus) : il faut créer 3 sections. Chaque section possède 3 div. Chaque div possède 3 paragraphes (avec du texte). Chaque paragraphe possède 3 spans (avec du texte et une classe, la même pour chaque span)
+ * -------------------------------------------------------
+ */
+
+for (let i = 0; i < 3; i++) {
+  //boucle 3 fois pour créer trois sections //
+
+  let section = document.createElement("section"); //création d'une section
+  document.body.appendChild(section); // ajouter la section au body
+
+  for (let j = 0; j < 3; j++) {
+    //boucle 3 fois pour créer trois divs //
+
+    let div = document.createElement("div"); //création de la div
+    section.appendChild(div); // ajouter la div à la section
+
+    let p = document.createElement("p"); // création du paragraphe
+    div.appendChild(p); // ajouter le paragraphe à la div
+
+    let span = document.createElement("span"); // création du span
+    span.id = "je suis un span";
+    p.appendChild(span); // ajouter le span au paragraphe
+  }
+}
 
 /**
-   * -------------------------------------------------------
-   * 8 - Sélectionner tous les spans nouvellement créés. Modifier leur texte via une boucle. Chaque span doit afficher "je suis le span n°X"
-   * Exemple : 
-   * → Le premier span doit afficher "je suis le span n°1"
-   * → Le second span doit afficher "je suis le span n°2"
-   * → etc... jusqu'au span n°27 🤯
-   * -------------------------------------------------------
-*/
+ * -------------------------------------------------------
+ * 8 - Sélectionner tous les spans nouvellement créés. Modifier leur texte via une boucle. Chaque span doit afficher "je suis le span n°X"
+ * Exemple :
+ * → Le premier span doit afficher "je suis le span n°1"
+ * → Le second span doit afficher "je suis le span n°2"
+ * → etc... jusqu'au span n°27 🤯
+ * -------------------------------------------------------
+ */
+
+let arraySpans = document.querySelectorAll("span"); // sélection de tous les spans qui devient une NodeList de neuf éléments
+
+console.log(arraySpans); // vérification
+
+for (let i = 0; i < arraySpans.length; i++) {
+  arraySpans[i].innerText = `je suis le span n°${i + 1}`;
+}
+console.log(arraySpans);
