4970ab1562e804921b397f56e09d71f5e7a98593
Claire Naudin <cndweb37@gmail.com>
2025-01-09 09:00:36 +0100

    mise à jour de l'importation de ReactDOM et simplification des commentaires dans le fichier index.js pour les exercices RxJS

--- Greta/Cours/2501/250108RxJs-exercices-applicatifs/exo-1-rxjs-observable/src/index.js
+++ Greta/Cours/2501/250108RxJs-exercices-applicatifs/exo-1-rxjs-observable/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Utilisez 'react-dom/client' pour React 18
+import ReactDOM from "react-dom/client"; 
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -10,15 +10,12 @@ import { map } from "rxjs/operators";
 document.getElementById("ex1").addEventListener("click", () => {
   const result1 = document.getElementById("result1");
 
-  // Créer un observable qui émet une valeur toutes les secondes
   const observable = interval(1000).pipe(map((value) => `Valeur : ${value}`));
 
-  // S'abonner à l'observable et mettre à jour le div
   const subscription = observable.subscribe((value) => {
     result1.textContent = value;
   });
 
-  // Pour arrêter l'observable après 10 secondes
   setTimeout(() => {
     subscription.unsubscribe();
   }, 10000);
