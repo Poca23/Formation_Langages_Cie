340a7445883d9ef9715e079f4e5a9ebf617367a7
Claire Naudin <cndweb37@gmail.com>
2025-01-09 09:31:58 +0100

    Ajout d'un observable RxJS pour afficher des valeurs dynamiques sur clic, avec désabonnement après 6 secondes

--- Greta/Cours/2501/250108RxJs-exercices-applicatifs/exo-2-rxjs-observable/src/index.js
+++ Greta/Cours/2501/250108RxJs-exercices-applicatifs/exo-2-rxjs-observable/src/index.js
@@ -1,12 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; 
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { getCLS, getFID, getLCP, getFCP, getTTFB } from "web-vitals";
+import { interval } from "rxjs";
+import { map } from "rxjs/operators";
 
 document.getElementById("ex2").addEventListener("click", () => {
+  const result2 = document.getElementById("result2");
 
+  const observable = interval(1000).pipe(map((value) => `Valeur : ${value}`));
+
+  const subscription = observable.subscribe((value) => {
+    result2.textContent = value;
+  });
+
+  setTimeout(() => {
+    subscription.unsubscribe();
+  }, 6000);
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
