ca695a9386daf445a44011485dd361d4bb056123
Claire Naudin <cndweb37@gmail.com>
2025-01-11 16:27:13 +0100

    Ajout de la fonctionnalité de sections dépliantes avec JavaScript, mise à jour des fichiers HTML et CSS, et suppression des fichiers obsolètes.

---
+++ TutoReact/Chapitre-2-Presentation-de-React/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll(".collapsible-header").forEach((header) => {
+  header.addEventListener("click", () => {
+    const content = header.nextElementSibling;
+    content.style.display =
+      content.style.display === "block" ? "none" : "block";
+  });
+});
