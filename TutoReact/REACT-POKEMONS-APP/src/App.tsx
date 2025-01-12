dd1d8eae5cc74a4b8395e29479b15417be1c51f1
Claire Naudin <cndweb37@gmail.com>
2025-01-12 19:59:40 +0100

    Ajout de nouvelles sections et mise à jour des styles CSS pour améliorer la présentation du cours.

--- TutoReact/REACT-POKEMONS-APP/src/App.tsx
+++ TutoReact/REACT-POKEMONS-APP/src/App.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { FunctionComponent, useState } from "react";
+import Pokemon from "./models/pokemon";
+import POKEMONS from "./models/mock-pokemon";
 
-const App: React.FC = () => {
-  const name: String = "React";
+const App: FunctionComponent = () => {
+  const [projet] = useState<String>("Pokédex");
+  const [pokemons] = useState<Pokemon[]>(POKEMONS);
 
-  return <h1>Hello, {name} !</h1>;
+  return (
+  <div>
+    <h1>Voici mon projet de {projet} !</h1>
+    <h2>Pokédex</h2>
+    <p>Il y a {pokemons.length} pokémons dans le Pokédex.</p>
+  </div>
+  );
 };
 
 export default App;
