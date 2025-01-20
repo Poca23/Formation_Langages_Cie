2bbea5949539bec294b93a7003c903bee73400ed
Claire Naudin <cndweb37@gmail.com>
2025-01-20 15:59:46 +0100

    Ajout de la méthode checkLocation pour vérifier l'accès en fonction de l'âge et de la catégorie, et mise à jour de la méthode main pour l'utiliser.

--- Greta/Cours/2501/250120Java/types-primitifs/src/TypesPrimitifs.java
+++ Greta/Cours/2501/250120Java/types-primitifs/src/TypesPrimitifs.java
@@ -1,8 +1,20 @@
 public class TypesPrimitifs {
 
+    public static void checkLocation(int age, String categorie) {
+
+        if (age < 12) {
+            System.out.println("Accès refusé. Âge minimum : 12 ans.");
+        } else if (age > 12 && age <= 16 && "Horreur".equalsIgnoreCase(categorie)) {
+            System.out.println("Accès refusé. Catégorie interdite aux moins de 16 ans.");
+        } else {
+            System.out.println("\"Location autorisée.");
+        }
+
+    }
+
     public static void main(String[] args) {
-        
-
-
+        int age = 13;
+        String categorie = "Horreur";
+        checkLocation(age, categorie);
     }
 }
