82ca7ea20c7a8998771c2c580698ee17936b3937
Claire Naudin <cndweb37@gmail.com>
2025-01-20 15:00:18 +0100

    Suppression de l'ancienne classe types_primitifs et ajout de la nouvelle classe TypesPrimitifs avec des méthodes commentées pour la comparaison d'acteurs.

---
+++ Greta/Cours/2501/250120Java/types-primitifs/src/TypesPrimitifs.java
@@ -0,0 +1,21 @@
+public class TypesPrimitifs {
+
+    // Méthode pour comparer deux acteurs sans tenir compte des
+    // majuscules/minuscules
+    // public static boolean comparerActeurs(String acteur1, String acteur2) {
+    //     return acteur1.equalsIgnoreCase(acteur2);
+    
+    public static void main(String[] args) {
+        // Tests de la méthode comparerActeurs
+        // System.out.println(comparerActeurs("Leonardo DiCaprio", "leonardo dicaprio")); // Affiche true
+        // System.out.println(comparerActeurs("Brad Pitt", "Angelina Jolie")); // Affiche false
+
+        // String note1 = "8.5";
+        // String note2 = "9";
+        // String note3 = "9.5";
+        // System.out.println((Double.parseDouble(note1) + Double.parseDouble(note2) + Double.parseDouble(note3)) / 3);
+
+
+        
+    }
+}
