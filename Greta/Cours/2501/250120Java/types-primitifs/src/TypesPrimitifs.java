2f989d70b0b664038cbcacb6eaee77754b9c54d6
Claire Naudin <cndweb37@gmail.com>
2025-01-20 15:18:40 +0100

    Ajout de la méthode verifierIdentifiant pour comparer un identifiant sous forme de chaîne avec un entier, et amélioration des commentaires dans la méthode main.

--- Greta/Cours/2501/250120Java/types-primitifs/src/TypesPrimitifs.java
+++ Greta/Cours/2501/250120Java/types-primitifs/src/TypesPrimitifs.java
@@ -3,19 +3,36 @@ public class TypesPrimitifs {
     // Méthode pour comparer deux acteurs sans tenir compte des
     // majuscules/minuscules
     // public static boolean comparerActeurs(String acteur1, String acteur2) {
-    //     return acteur1.equalsIgnoreCase(acteur2);
-    
+    // return acteur1.equalsIgnoreCase(acteur2);
+
     public static void main(String[] args) {
         // Tests de la méthode comparerActeurs
-        // System.out.println(comparerActeurs("Leonardo DiCaprio", "leonardo dicaprio")); // Affiche true
-        // System.out.println(comparerActeurs("Brad Pitt", "Angelina Jolie")); // Affiche false
+        // System.out.println(comparerActeurs("Leonardo DiCaprio", "leonardo
+        // dicaprio")); // Affiche true
+        // System.out.println(comparerActeurs("Brad Pitt", "Angelina Jolie")); //
+        // Affiche false
 
         // String note1 = "8.5";
         // String note2 = "9";
         // String note3 = "9.5";
-        // System.out.println((Double.parseDouble(note1) + Double.parseDouble(note2) + Double.parseDouble(note3)) / 3);
+        // System.out.println((Double.parseDouble(note1) + Double.parseDouble(note2) +
+        // Double.parseDouble(note3)) / 3);
 
-
-        
+        System.out.println(verifierIdentifiant("123", 123)); // Affiche true
+        System.out.println(verifierIdentifiant("456", 789)); // Affiche false
     }
+
+    public static boolean verifierIdentifiant(String identifiantString, Integer identifiantRecherche) {
+        try {
+            int identifiantConverti = Integer.parseInt(identifiantString);
+
+            return identifiantConverti == identifiantRecherche;
+        } catch (NumberFormatException e) {
+            
+            System.out.println("Erreur : l'identifiantString n'est pas un nombre valide.");
+            return false;
+        }
+
+    }
+
 }
