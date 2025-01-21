161ce23fbfebb0f057f18b3db4949bd49d7915b7
Claire Naudin <cndweb37@gmail.com>
2025-01-21 17:04:43 +0100

    Ajout du chapitre 12 sur le développement d'une application avec Laravel, incluant la création d'une application CRUD, la gestion des routes et des contrôleurs, ainsi que des exemples de vues Blade. Refactorisation de la classe FilmService pour corriger l'ajout et la liste des films.

--- Greta/Cours/2501/250121Creation-Partie-Back/visotech-core-cnd/src/main/java/vcom/visiotech/domain/service/FilmService.java
+++ Greta/Cours/2501/250121Creation-Partie-Back/visotech-core-cnd/src/main/java/vcom/visiotech/domain/service/FilmService.java
@@ -1,6 +1,7 @@
 package vcom.visiotech.domain.service;
 
 import java.util.List;
+import java.util.ArrayList;
 import vcom.visiotech.domain.model.Film;
 
 public class FilmService {
@@ -8,16 +9,13 @@ public class FilmService {
     // méthode temporaire pour stocker un jeu de films en local, sans la DB
     private List<Film> films = new ArrayList<>();
 
-    // méthode temporaire pour stocker un jeu de films en local, sans la DB
-    public class FilmService {
-        public void addFilm(Film film) {
-            // Ajouter le film à la DataBase
-            films.add(newFilm);
-        }
+    public void addFilm(Film film) {
+        // Ajouter le film à la DataBase
+        films.add(film);
+    }
 
-        public List<Film> listFilms() {
-            // Récupérer les films depuis la DataBase
-            return films;
-        }
+    public List<Film> listFilms() {
+        // Récupérer les films depuis la DataBase
+        return films;
     }
 }
