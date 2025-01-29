08bfd9799d0e0bb405c0a266831a6c015141bcd1
Naudin <cndweb37@gmail.com>
2025-01-29 16:32:31 +0100

    Initialisation du projet Spring Boot avec configuration Maven, fichiers de propriétés, et tests de contexte

---
+++ Greta/Cours/2501/250129Spring-Boot/Exemple-projectcnd-SpringBoot/src/main/java/org/cnd/projectcnd/ProjectCndApplication.java
@@ -0,0 +1,13 @@
+package org.cnd.projectcnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCndApplication.class, args);
+	}
+
+}
---
+++ Greta/Cours/2501/250129Spring-Boot/Exemple-projectcnd-SpringBoot/src/test/java/org/cnd/projectcnd/ProjectCndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cnd.projectcnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
